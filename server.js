06c234bc45f3f75d3fbcc2f38f915875d421bbec
akonovalov <akonovalov@at-consulting.ru>
2017-06-24 13:35:58 +0400

    add GET services for server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const compression = require('compression');
 const express = require('express');
 const config = require('./config');
 
@@ -5,12 +6,40 @@ const app = express();
 
 app.set('port', config.PORT);
 
-app.use('/', express.static(`${__dirname}/dist/`));
+// Gzip
+app.use(compression());
 
-app.get('/backend', (req, res) => {
-  res.send('works');
+// Get stats
+app.get('/stats/:smartUrlId', (req, res) => {
+  // TODO: get stats from service and return stats
+  res.send(req.params.smartUrlId);
 });
 
+// Get smart-link /make?url=http://google.com
+app.get('/make', async (req, res) => {
+  let smartLink;
+  if (req.query.from === 'body') {
+    // TODO: add async service TEXT MINING
+    smartLink = 'from url html body';
+  } else {
+    // TODO: async get from title
+    smartLink = 'from url title';
+  }
+  // TODO: saveToDB(smartLink, req.query.url);
+  res.send(smartLink);
+});
+
+// TODO: check exclude /make or /stats
+app.get('/:smartUrlId', async (req, res) => {
+  const redirectUrl = ''; // TODO:  = getFromDB(req.params.smartUrlId)
+  res.send(redirectUrl || req.params.smartUrlId);
+});
+
+// Run the app by serving the static files
+// in the dist directory
+app.use('/', express.static(`${__dirname}/dist/`));
+
+// Start the app by listening on the default port
 app.listen(app.get('port'), () => {
   console.log('App is running, server is listening on port:', app.get('port'));
 });
